2e37d1953179f9f6762fbf301030a7e763d9ef8a
Bruce McClure <brucemcc90@gmail.com>
2019-12-18 12:35:51 +1100

    a

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // import the students from the students file.
-// consolelog your name
+// 1) consolelog your name
 
-// console.log(`Hi my name is ${students[10].firstName}`)
+// 2) console.log(`Hi my name is ${students[10].firstName}`)
 
-// Loop over the entire array and print every first name
+// 3) Loop over the entire array and print every first name
 
-// make function to push all the emails to an array.
+// 4) make function to push all the emails to an array.
 
-// Make a function to add an age property to everyone
+// 5) Make a function to add an age property to everyone
 
-// Check the data types of the values
+// 6) Check the data types of the values
